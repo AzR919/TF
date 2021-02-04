51cdcd23aa1db53dd36d74365d190bf27d720cbe
Azaan Rehman <azaanrehman919@gmail.com>
2021-02-04 07:24:44 -0500

    FInished simple neural net

--- tf_prac.py
+++ tf_prac.py
@@ -1,9 +1,97 @@
-#%%
+# %%
+# Set imports
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
+
+from tensorflow import keras
+
+# %%
+# Load Data
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+# Set class names
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+# Preprocess
+train_images = train_images / 255.0
+
+test_images = test_images / 255.0
+
+# %%
+# Model
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),  # input layer (1)
+    keras.layers.Dense(128, activation='relu'),  # hidden layer (2)
+    keras.layers.Dense(10, activation='softmax') # output layer (3)
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# %%
+# Training
+
+model.fit(train_images, train_labels, epochs=10)
+# %%
+# Evaluate
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=1) 
+
+print('Test accuracy:', test_acc)
+# %%
+# Make predictions
+
+predictions = model.predict(test_images) # Just an array
+
+class_i = np.argmax(predictions[0])
+
+print(class_names[class_i])
+# %%
+# Verification
+
+COLOR = 'black'
+plt.rcParams['text.color'] = COLOR
+plt.rcParams['axes.labelcolor'] = COLOR
+
+def predict(model, image, correct_label):
+  class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+  prediction = model.predict(np.array([image]))
+  predicted_class = class_names[np.argmax(prediction)]
+
+  show_image(image, class_names[correct_label], predicted_class)
 
 
-#%%
-tf_data = tf.zeros((4,5))
+def show_image(img, label, guess):
+  plt.figure()
+  plt.imshow(img, cmap=plt.cm.binary)
+  plt.title("Excpected: " + label)
+  plt.xlabel("Guess: " + guess)
+  plt.colorbar()
+  plt.grid(False)
+  plt.show()
+
+
+def get_number():
+  while True:
+    num = input("Pick a number: ")
+    if num.isdigit():
+      num = int(num)
+      if 0 <= num <= 1000:
+        return int(num)
+    else:
+      print("Try again...")
+
+num = get_number()
+image = test_images[num]
+label = test_labels[num]
+predict(model, image, label)
 
 # %%
